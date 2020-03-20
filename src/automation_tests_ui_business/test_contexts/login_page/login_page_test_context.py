943910f130bbbb9387c32c1196b572b05519f462
EugeneKiselev <eugene.kiselev@oxagile.com>
2020-03-20 16:37:56 +0300

    added logging

--- src/automation_tests_ui_business/test_contexts/login_page/login_page_test_context.py
+++ src/automation_tests_ui_business/test_contexts/login_page/login_page_test_context.py
@@ -1,4 +1,5 @@
 from src.automation_tests_ui.pages.login.login_page import LoginPage
+import logging
 
 
 class LoginPageTestContext:
@@ -7,10 +8,19 @@ class LoginPageTestContext:
         self.loginPage = LoginPage(driver)
 
     def register(self, firstName, lastName, email, company):
+        logging.info("Enter First Name = '{0}'".format(firstName))
         self.loginPage.loginForm.firstNameInput.send_keys(firstName)
+
+        logging.info("Enter Last Name = '{0}'".format(lastName))
         self.loginPage.loginForm.lastNameInput.send_keys(lastName)
+
+        logging.info("Enter Email = '{0}'".format(email))
         self.loginPage.loginForm.emailInput.send_keys(email)
+
+        logging.info("Enter Company = '{0}'".format(company))
         self.loginPage.loginForm.companyInput.send_keys(company)
+
+        logging.info("Click 'Register' button")
         self.loginPage.loginForm.registerButton.click()
 
     def is_registration_form_displayed(self):
--- tests/ui/test_base.py
+++ tests/ui/test_base.py
@@ -1,7 +1,11 @@
 from src.testframework_ui.selenium_driver import SeleniumDriver
+import logging
+from src.testframework_ui.utils.config_parser import ConfigParser
 
 
 class BaseTestClass:
+    logging.basicConfig(filename='logs.log', level=ConfigParser.get_settings().get("LOGGER", "Loglevel"))
+    logging.FileHandler(filename='logs.log', mode='w')
 
     @classmethod
     def setup_class(cls):
