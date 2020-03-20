38ca56e26cdf40812300590193bb86e8b96b1d71
EugeneKiselev <eugene.kiselev@oxagile.com>
2020-03-20 10:01:16 +0300

    added asserst for api tests

--- tests/api/test_bm.py
+++ tests/api/test_bm.py
@@ -1,10 +1,13 @@
 import requests
 import json
 
+
 class TestBm:
 
+    __key = "name"
+    __value = "ironman"
     __url = "https://httpbin.org/anything"
-    __test_json = "{\"name\":\"ironman\"}"
+    __test_json = "{{\"{0}\": \"{1}\"}}".format(__key, __value)
 
     def test_get_request_response_status_code_200(self):
         response = requests.get(self.__url)
@@ -12,8 +15,19 @@ class TestBm:
 
     def test_get_request_response_body_is_correct(self):
         response = requests.get(self.__url)
-        body = response.text
-        assert body == ""
+        json_data = json.loads(response.text)
+        headers = json_data["headers"]
+
+        assert json_data["args"] == {}
+        assert json_data["data"] == ""
+        assert json_data["files"] == {}
+        assert json_data["form"] == {}
+        assert headers["Accept"] == '*/*'
+        assert headers["Accept-Encoding"] == 'gzip, deflate'
+        assert headers["Host"] == 'httpbin.org'
+        assert json_data["json"] is None
+        assert json_data["method"] == "GET"
+        assert json_data["url"] == self.__url
 
     def test_post_request_response_status_code_200(self):
         response = requests.post(self.__url, self.__test_json)
@@ -21,13 +35,17 @@ class TestBm:
 
     def test_post_request_response_body_is_correct(self):
         response = requests.post(self.__url, self.__test_json)
-        body = response.text
-        json_data = json.loads(body)
+        json_data = json.loads(response.text)
+        headers = json_data["headers"]
 
         assert json_data["args"] == {}
+        assert json_data["data"] == self.__test_json
         assert json_data["files"] == {}
         assert json_data["form"] == {}
-        assert json_data["data"] == self.__test_json
-        assert json_data["json"] == self.__test_json
+        assert headers["Accept"] == '*/*'
+        assert headers["Accept-Encoding"] == 'gzip, deflate'
+        assert headers["Content-Length"] == '{0}'.format(len(self.__test_json))
+        assert headers["Host"] == 'httpbin.org'
+        assert json_data["json"] == {'{0}'.format(self.__key): '{0}'.format(self.__value)}
         assert json_data["method"] == "POST"
         assert json_data["url"] == self.__url
