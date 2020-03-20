ecef4ffffefaab50d71a385f7245fa15f326b841
EugeneKiselev <eugene.kiselev@oxagile.com>
2020-03-20 11:39:54 +0300

    get base url from test settings file

--- src/automation_tests_ui/pages/home/home_page.py
+++ src/automation_tests_ui/pages/home/home_page.py
@@ -1,10 +1,10 @@
 from src.automation_tests_ui.pages.base_page import BasePage
 from selenium.webdriver.common.by import By
+from src.testframework_ui.utils.config_parser import ConfigParser
 
 
 class HomePage(BasePage):
-
-    __url = "https://www.www-bm-qa-base.blazemeter.net/"
+    __section = "DEFAULT"
     __startTestingNowButtonLocator = ".//button[@class='get_started_button home_get_started_button']"
 
     def __init__(self, driver):
@@ -15,4 +15,7 @@ class HomePage(BasePage):
         return BasePage.find_element(self, By.XPATH, self.__startTestingNowButtonLocator)
 
     def navigate(self):
-        BasePage.navigate(self, self.__url)
+        settings = ConfigParser().settings
+        base_url = settings.get(self.__section, "BaseUrl")
+
+        super().navigate(base_url)
