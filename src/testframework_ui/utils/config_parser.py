922a36ba2287ebc68a21c72b6bc556c8195654b2
EugeneKiselev <eugene.kiselev@oxagile.com>
2020-03-20 13:15:35 +0300

    added abs path for test settings file

--- src/testframework_ui/utils/config_parser.py
+++ src/testframework_ui/utils/config_parser.py
@@ -1,14 +1,15 @@
 import configparser
+import os
 
 
 class ConfigParser:
-    __config_file_path = r"D:\!BmTests\bm-tests\tests\ui\test_settings.cfg"
     config = None
+    abs_path = os.path.abspath("test_settings.cfg")
 
     @staticmethod
     def get_settings():
         if ConfigParser.config is None:
             ConfigParser.config = configparser.RawConfigParser()
-            ConfigParser.config.read(ConfigParser.__config_file_path)
+            ConfigParser.config.read(ConfigParser.abs_path)
 
         return ConfigParser.config
